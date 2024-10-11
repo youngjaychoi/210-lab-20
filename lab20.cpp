bf0beaa1bcc4439688332aa58a3acaa51ff1111a
youngjaychoi <youngjaychoi02@gmail.com>
2024-10-10 20:57:23 -0700

    commit5

--- lab20.cpp
+++ lab20.cpp
@@ -63,25 +63,18 @@ int main() {
 
     //creating pointer to first chair object
     Chair *chairPtr = new Chair;
-    // chairPtr->setLegs(4);
-    // chairPtr->setPrices(121.21, 232.32, 414.14);
     chairPtr->print();
 
     //creating dynamic chair object with constructor
-    Chair *livingChair = new Chair(3);
-    livingChair->setPrices(525.25, 434.34, 252.52);
+    double prices[SIZE] = (525.25, 434.34, 252.52);
+    Chair *livingChair = new Chair(3, prices);
     livingChair->print();
     delete livingChair;
     livingChair = nullptr;
 
     //creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
+    
     for (int i = 0; i < SIZE; i++) {
         collection[i].print();
     }
